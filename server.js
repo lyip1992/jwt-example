10079dd7e75a2dc78c51e7924284aa752fdde6dd
Leon Yip <lyip1992@gmail.com>
2015-07-16 16:11:31 -0700

    create api route to /

--- server.js
+++ server.js
@@ -39,5 +39,16 @@ app.get('/setup', function(req, res){
 
 var apiRoutes = express.Router(); // borrow the express router
 
+// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
+
+// TODO: route middleware to verify a token
+
+// route to show a random message (GET http://localhost:8080/api/)
+apiRoutes.get('/', function(req, res){
+  res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+app.use('/api', apiRoutes);
+
 app.listen(port);
 console.log('Express server listening on port: ' + port);
