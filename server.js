5460f50a4953bdea23658c5c197d74a60282e63c
Leon Yip <lyip1992@gmail.com>
2015-07-16 15:28:46 -0700

    add comments, set port variable, connect to mongo, use secret from config

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ var app = require('express')();
 var body-parser = require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
-var jwt = require('jsonwebtoken');
-var config = require('./config');
-var User = require('app/models/user');
+
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./config'); // get conifguration file
+var User = require('app/models/user'); // get user mongoose model
+
+var port = process.env.PORT || 8080; // create, sign, and verify tokens
+mongoose.connect(config.database); // connect to the database
+app.use('secret', config.secret); // secret variable
