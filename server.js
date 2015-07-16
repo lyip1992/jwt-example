cb90ddfa7875903da36a08546f665bbb457c8753
Leon Yip <lyip1992@gmail.com>
2015-07-16 15:41:54 -0700

    fix typos

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var mongoose = require('mongoose');
 
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('./config'); // get conifguration file
-var User = require('app/models/user'); // get user mongoose model
+var User = require('./app/models/user'); // get user mongoose model
 
 var port = process.env.PORT || 8080; // create, sign, and verify tokens
 mongoose.connect(config.database); // connect to the database
-app.use('superSecret', config.secret); // secret variable
+app.set('superSecret', config.secret); // secret variable
 
 // use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false }));
