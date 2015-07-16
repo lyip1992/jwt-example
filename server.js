fdf477105cf6d0165f228414a653b68f38517b70
Leon Yip <lyip1992@gmail.com>
2015-07-16 16:49:07 -0700

    added api route middleware to check tokens

--- server.js
+++ server.js
@@ -63,7 +63,25 @@ apiRoutes.post('/authenticate', function(req, res){
   });
 });
 
-// TODO: route middleware to verify a token
+apiRoutes.use(function(req, res, next){
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  if( token ){
+    jwt.verify(token, app.get('superSecret'), function(err, decoded){
+      if( err ){
+        return res.json({ success: false, message: 'Failed to authenticate token.' });
+      } else {
+        req.decoded = decoded; // if everything is good, then save to request for use in other routes
+        next();
+      }
+    });
+  } else { // if there is no token then return an error
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided'
+    });
+  }
+});
 
 // route to show a random message (GET http://localhost:8080/api/)
 apiRoutes.get('/', function(req, res){
