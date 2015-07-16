d37d1cf4ed8f19ca32019002361a8fd389a1886d
Leon Yip <lyip1992@gmail.com>
2015-07-16 16:06:33 -0700

    refactored comments and assigned apiRouter

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
@@ -15,31 +16,28 @@ app.set('superSecret', config.secret); // secret variable
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// use morgan to log requests to the console
-app.use(morgan('dev'));
-
-// routes
+app.use(morgan('dev')); // use morgan to log requests to the console
 
 app.get('/', function(req, res){
   res.send('Hello! This is the API at http://localhost:' + port + '/api');
 });
 
 app.get('/setup', function(req, res){
-  // create a sample user
-  var leon = new User({
+  var leon = new User({ // create a sample user
     name: 'Leon',
     password: 'password',
     admin: true
   });
 
-  // save sample user
-  leon.save(function(err){
+  leon.save(function(err){ // save sample user
     if( err ) throw err;
 
     console.log('User saved successfully.');
     res.json({ success: true });
   });
 });
+
+var apiRoutes = express.Router(); // borrow the express router
 
 app.listen(port);
 console.log('Express server listening on port: ' + port);
